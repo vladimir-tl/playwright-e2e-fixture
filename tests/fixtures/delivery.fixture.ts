f43e61d4fc7019c3d1ab5eae0dd3c1777990dfc7
vladimirovodenko <vladimir@tallinn-learning.ee>
2024-12-09 18:15:46 +0200

    fixture added with mock and reusing another fixture

--- tests/fixtures/delivery.fixture.ts
+++ tests/fixtures/delivery.fixture.ts
@@ -1,11 +1,12 @@
-import { test as base } from '@playwright/test'
-import { BASE_API, loginPath, orderPath } from '../../config/env-data'
+import { Page, test as base } from '@playwright/test'
+import { BASE_API, loginPath, orderPath, SERVICE_URL } from '../../config/env-data'
 import { LoginDto } from '../dto/login-dto'
 import { OrderDto } from '../dto/order-dto'
 
 type Fixtures = {
   auth: { jwt: string }
   orderId: string
+  mainPage: Page
 }
 
 export const test = base.extend<Fixtures>({
@@ -33,6 +34,34 @@ export const test = base.extend<Fixtures>({
     const orderId = responseData.id
     console.log('order created with id: ', orderId)
     await use(orderId)
+  },
+
+  mainPage: async ({ context, auth }, use) => {
+
+    // Set JWT in localStorage
+    await context.addInitScript((token) => {
+      localStorage.setItem('jwt', token)
+    }, auth.jwt)
+
+    const mainPage = await context.newPage()
+
+    await mainPage.route(`${BASE_API}${orderPath}/*`, route =>
+      route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({
+          status: 'DELIVERED',
+          courierId: null,
+          customerName: 'mocked customer',
+          customerPhone: '99887766',
+          comment: '',
+          id: 9999,
+        }),
+      })
+    );
+
+    await mainPage.goto(SERVICE_URL)
+    await use(mainPage);
   },
 })
 
